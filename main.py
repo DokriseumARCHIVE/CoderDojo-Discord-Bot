72c57cf53f954ea51dcbff9934509238360b2e55
Bluemi <bruno.schilling@protonmail.ch>
2022-01-14 19:03:11 +0100

    fix project dir identification

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 
 # This is a sample Python script.
 
-# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -21,7 +22,9 @@ class MyClient(discord.Client):
             channel = client.get_channel(916648708655951887) # .channel.id)
             await channel.send(f"Hallo\n")
 
-project_dir = os.path.dirname(__file__)
+
+project_dir = os.path.dirname(os.path.realpath(__file__))
+print(f'project dir: {project_dir}')
 with open(f"{project_dir}/config/key.txt", "r") as file:
     key = file.read().replace("\n", "")
 
